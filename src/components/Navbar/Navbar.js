ebb97ec7e3ac36560d3da7b17b0081341ce7c958
Jethro Au <supergalong@gmail.com>
2021-12-10 04:09:58 +0000

    update logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -27,11 +27,7 @@ const Navbar = () => {
     >
       <div className="nav-left">
         <a href="/">
-          <img
-            className="nav-logo"
-            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-            alt="Netflix"
-          />
+          <img className="nav-logo" src="/assets/logo.png" alt="Netflix" />
         </a>
         <a href="#" className="nav-left-links">
           Home
